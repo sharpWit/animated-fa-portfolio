70248ea3c5bc5257bef19a674b8f3e832522cc15
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-10-11 17:52:51 +0330

    First initializarion

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <section>
+        <Navbar />
+      </section>
+      <section>Parallax</section>
+      <section>services</section>
+      <section>Parallax</section>
+      <section>Portfolio</section>
+      <section>Portfolio</section>
+      <section>Portfolio</section>
+      <section>Portfolio</section>
+      <section>Contact</section>
+    </div>
+  );
+};
+
+export default App;
